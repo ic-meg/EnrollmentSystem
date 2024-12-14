b591b228ca6afed51dd9aeae00aa9112848326f3
Meg Angeline Fabian <ic.megangeline.fabian@cvsu.edu.ph>
2024-12-15 03:47:00 +0800

    Update admin-template.php

--- EnrollmentSystem/admin/admin-template.php
+++ EnrollmentSystem/admin/admin-template.php
@@ -10,11 +10,12 @@
 
 
 <main>
-    <!--Content here-->
-  <!-- WAG TANGGALIN MAIN AT CONTAINER-->
+    
+    <!-- WAG TANGGALIN MAIN AT CONTAINER-->
     </div>
     </div>
     <div class="container">
+      <!--Content here-->
       <h2>Hello World</h2>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis porro iure quaerat aliquam! Optio dolorum in eum provident, facilis error repellendus excepturi enim dolor deleniti adipisci consectetur doloremque, unde maiores odit sapiente. Atque ab necessitatibus laboriosam consequatur eius similique, ex dolorum eum eaque sequi id veritatis voluptates perspiciatis, cupiditate pariatur.</p>
     </div>
